b649a509f11d1ddf65682e930971321825b4ace3
Ali093 <alishasakshi.rose30@gmail.com>
2021-09-18 02:24:27 +0100

    add port changes

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const userRouter = require('./apis/router');
+const server_port = process.env.PORT || 5000 ;
 
 app.use(express.json());
 app.use('/',userRouter); 
 
-app.listen(process.env.APP || 5000, () => {                     //app.listen(5000,function() {})
+app.listen(server_port, () => {                     //app.listen(5000,function() {})
     console.log("server up and running on PORT:",process.env.PORT );
 });
 
