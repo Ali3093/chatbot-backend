ebc5b3787cf9350d97b939f320e7953dfb63f00e
Ali093 <alishasakshi.rose30@gmail.com>
2021-09-18 02:13:58 +0100

    add changes

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const userRouter = require('./apis/router');
 app.use(express.json());
 app.use('/',userRouter); 
 
-app.listen(process.env.APP_PORT, () => {                     //app.listen(5000,function() {})
-    console.log("server up and running on PORT:",process.env.APP_PORT);
+app.listen(process.env.APP || 5000, () => {                     //app.listen(5000,function() {})
+    console.log("server up and running on PORT:",process.env.PORT );
 });
 
 
